0c70e49a024056653ecf04a0bd227d56eafe2e89
“soudanieldias” <danieldias.b99@hotmail.com>
2022-07-17 02:45:13 -0300

    feat: add generatereport command v1

---
+++ src/commands/generatereport.ts
@@ -0,0 +1,38 @@
+import { Client, GuildMember, Message } from "discord.js";
+import fs from 'fs/promises';
+
+module.exports.run = async (client:Client, message:Message) => {
+  const userPerms = message.member!.guild.me?.permissions.toArray();
+  const hasAdminRole = userPerms?.some((role) => (role == "ADMINISTRATOR"));
+
+  if(!hasAdminRole) return message.reply('Erro: Não Autorizado!');
+
+  const guild = client.guilds.resolve(message.guildId!);
+  const guildMembers = await guild!.members.fetch();
+  const [ users, bots ] = [
+    guildMembers.filter((member) => !member.user.bot),
+    guildMembers.filter((member) => member.user.bot),
+  ];
+
+  const timeStamp = new Date().getTime();
+  
+  const botMessage = await message.channel.send({content: 'Iniciando a criação do Relatório de Membros do servidor'});
+
+  // Cria Diretório
+  await fs.mkdir(`./src/reports/${timeStamp}`);
+
+  // Cria LOG de USUÁRIOS
+  await fs.writeFile(`./src/reports/${timeStamp}/users.txt`, `REPORT DE USUÁRIOS: ${timeStamp}\n`);  
+  users.forEach(async (user) => {
+    await fs.appendFile(`./src/reports/${timeStamp}/users.txt`, `${JSON.stringify(user)}\n`);
+  });
+
+  // Cria LOG de BOTS
+  await fs.writeFile(`./src/reports/${timeStamp}/bots.txt`, `REPORT DE BOTS: ${timeStamp}\n`);
+  bots.forEach(async (bot) => {
+    await fs.appendFile(`./src/reports/${timeStamp}/bots.txt`, `${JSON.stringify(bot)}\n`);
+  })
+
+  await botMessage.edit({ content: `Gerando Relatório ${timeStamp}` });
+  botMessage.edit({ content: `Tudo ok! ID do Relatório: ${timeStamp}`});
+};
