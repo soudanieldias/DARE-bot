faf470b3a40d85155581d6a7fb34d40321c05357
“soudanieldias” <danieldias.b99@hotmail.com>
2022-07-17 05:23:53 -0300

    refactor: call the new commandWatcher in App constructor

--- src/App.ts
+++ src/App.ts
@@ -31,7 +31,7 @@ export default class App {
     DatabaseConnection.databaseHandler();
     Interactions.buttonInteraction(this.client);
     CommandHandler.loadCommands(this.client);
-    // this.commandHandler();
+    CommandHandler.commandWatcher(this.client);
     this.joinHandler();
     this.voiceHandler();
   }
@@ -51,38 +51,6 @@ export default class App {
 
     this.client.login(this.TOKEN);
   }
-
-  // private commandHandler () {
-  //   this.client.on('messageCreate', async (message) => {
-  //     if ( 
-  //       message.author.bot
-  //       || !message.guild
-  //       || message.channel.type === 'DM'
-  //       || !message.content.startsWith(this.BOT_PREFIX)
-  //       || message.content.startsWith(`<@!${this.client.user!.id}`)
-  //       || message.content.startsWith(`<@${this.client.user!.id}`)
-  //       ) return;
-
-  //     const args = message.content.slice(this.BOT_PREFIX.length).trim().split(/ +/g);
-
-  //     if (!args) {
-  //       message.channel.send('Erro: Comando não Identificado!');
-  //       return;
-  //     }
-
-  //     const command = args.shift()!.toLowerCase();
-
-  //     try {
-  //       const commandFile = require(`./commands/${command}.ts`);
-  //       delete require.cache[require.resolve(`./commands/${command}.ts`)];
-  //       commandFile.run(this.client, message, args);
-
-  //     } catch (err) {
-  //       message.channel.send('Erro: Comando Não Encontrado!');
-  //       console.error(`Erro ao Digitar o Comando: '${command}' \n[DEPURAÇÃO] Erro Retornado: '${err}'`); // [DEBUG]
-  //     }
-  //   });
-  // }
 
   private joinHandler () {
     this.client.on('guildMemberAdd', async (user) => {
