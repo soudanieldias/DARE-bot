f64b90e104f176d83dfd282d06fa38b285a4df68
“soudanieldias” <danieldias.b99@hotmail.com>
2022-07-18 19:22:51 -0300

    feat: add log to SetActivity

--- src/events/SetActivity.ts
+++ src/events/SetActivity.ts
@@ -3,10 +3,14 @@ import { Client } from "discord.js";
 export default class SetActivity {
   public static setActivity(client:Client) {
     client.once('ready', () => {
+      console.log('[ACTIVITY] Carregando activity do BOT');
+      
       client.user!.setActivity(
         'BOT Online',
         { type: 'STREAMING', url: 'https://www.diasitservices.com.br/dare-bot' }
-      );
+        );
+
+      console.log('[ACTIVITY] Activity Carregada com Sucesso!');
     });
   }
 }
