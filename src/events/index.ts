d58e3950259ee325eedd981e56d55a33d938753b
“soudanieldias” <danieldias.b99@hotmail.com>
2022-07-25 20:32:21 -0300

    feat: add OnInteraction event

--- src/events/index.ts
+++ src/events/index.ts
@@ -2,5 +2,6 @@ import LoadCommands from './LoadCommands';
 import SetActivity from './SetActivity';
 import OnReady from './OnReady';
 import OnMessageCreate from './OnMessageCreate';
+import OnInteraction from './OnInteraction';
 
-export { LoadCommands, SetActivity, OnReady, OnMessageCreate };
+export { LoadCommands, SetActivity, OnReady, OnMessageCreate, OnInteraction };
