818ccc523216908fc0740ae2fa73a11297afe53b
“soudanieldias” <danieldias.b99@hotmail.com>
2022-07-25 19:21:19 -0300

    feat: import OnMessageCreate module

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,5 +1,6 @@
 import LoadCommands from './LoadCommands';
 import SetActivity from './SetActivity';
 import OnReady from './OnReady';
+import OnMessageCreate from './OnMessageCreate';
 
-export { LoadCommands, SetActivity, OnReady };
+export { LoadCommands, SetActivity, OnReady, OnMessageCreate };
