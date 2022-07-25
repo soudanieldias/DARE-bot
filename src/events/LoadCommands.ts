a3bfd82a8b7efd4bc6dda0ceddfc6267112136f8
“soudanieldias” <danieldias.b99@hotmail.com>
2022-07-25 17:49:11 -0300

    feat: add LoadCommands event

---
+++ src/events/LoadCommands.ts
@@ -0,0 +1,31 @@
+import { Client } from 'discord.js';
+import glob from 'glob';
+import App from '../App';
+
+export default async (client:Client) => {
+  try {
+    console.log('[Commands] Carregando Módulo de Comandos');
+    const commandFiles = glob.sync('./src/commands/**/*.ts');
+
+    for await (const file of commandFiles) {
+      const command = require(`../../${file}`);
+      const { name, category, description } = command;
+  
+      if (name) throw new Error(`[Erro]: Nome do Comando não encontrado! Arquivo: ${command}`);
+      if (category) return console.warn(`[Warn]: Categoria não encontrada! Arquivo: ${file}`);
+      if (description) return console.warn(`[Warn]: Description não encontrada! Arquivo: ${file}`);
+
+      const cmd = client.application?.commands.cache.find((c) => (c.name === command.name));
+
+      if (cmd) return console.error('[Erro] Já existe um comando carregado com o mesmo nome');
+
+      delete require.cache[require.resolve(`../../${file}`)];
+
+      App.commands.set(command.name, command);
+    }
+    console.log('[Comamnds] Módulo de Comandos Carregado com Sucesso');
+  } catch (error) {
+    console.error(error);
+    console.log('[Commands] [Erro] Ocorreu um Erro ao Carregar os Comandos');
+  }
+}
--- src/events/index.ts
+++ src/events/index.ts
@@ -1,4 +1,5 @@
+import LoadCommands from './LoadCommands';
 import SetActivity from './SetActivity';
 import OnReady from './OnReady';
 
-export { SetActivity, OnReady };
+export { LoadCommands, SetActivity, OnReady };
