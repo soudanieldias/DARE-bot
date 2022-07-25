f426d12fdc68612c26819b779adf262ee14f66d7
“soudanieldias” <danieldias.b99@hotmail.com>
2022-07-25 10:33:58 -0300

    feat: add SetActivity event

---
+++ src/events/SetActivity.ts
@@ -0,0 +1,18 @@
+import { Client } from 'discord.js';
+
+export default class setActivity {
+  public static default(client:Client) {
+    client.once('ready', () => {
+      console.log('[Activity] Inicializando Activity do BOT.');
+
+      client.user?.setActivity(
+        'BOT Online',
+        {
+          type: 'STREAMING',
+          url: 'https://diasitservices.com.br/'
+        });
+
+      console.log('[Activity] Activity Carregada com Sucesso.');
+    });
+  }
+}
