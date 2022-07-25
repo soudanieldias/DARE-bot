278f922df7216c347d2c516378cff670f3dfcdfd
“soudanieldias” <danieldias.b99@hotmail.com>
2022-07-25 10:34:11 -0300

    feat: add OnReady event

---
+++ src/events/OnReady.ts
@@ -0,0 +1,19 @@
+import { Client } from 'discord.js';
+
+export default (client:Client, token:string | undefined) => {
+  client.on('ready', () => {
+    console.log(`
+      Bot Online!
+      Username: ${client.user?.tag}
+      ------------------------------
+      Pronto para o Trabalho! Online para ${client.users.cache.size} Usuários.
+      Operando em ${client.guilds.cache.size} Servidores
+      ------------------------------
+      Lista de Guilds:
+      ${client.guilds.cache.map((guild) => guild.name).join('\n')}
+      ------------------------------
+    `)
+  });
+
+  client.login(token);
+}
