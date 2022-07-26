dd5bbcabefade371dce0f302a381212195d17fb7
“soudanieldias” <danieldias.b99@hotmail.com>
2022-07-26 04:54:27 -0300

    feat: add staff commands

---
+++ src/commands/staff/clearmessages.ts
@@ -0,0 +1,28 @@
+import { Client, Interaction, Message, TextChannel } from "discord.js";
+
+module.exports = {
+	name: 'clearmessages',
+	description: 'Clear a \'n\' quantity of messages in specified channel [STAFF]',
+	category: 'staff',
+	execute: async (_client:Client, message:Message, args:Array<string>) => {
+    try {
+      const userPerms = message.member!.guild.me?.permissions.toArray();
+      const hasAdminRole = userPerms?.some((role) => (role == "ADMINISTRATOR"));
+      const MESSAGES_TO_DELETE = Number(args[0]);
+
+      if (!hasAdminRole) return message.reply('ERRO: Não Autorizado!!!');
+
+      if(!MESSAGES_TO_DELETE || MESSAGES_TO_DELETE > 100 || MESSAGES_TO_DELETE < 1 ) {
+        return message.reply({ content: 'Quantidade de mensagens à apagar Inválida!' });
+      }
+
+      await (message.channel as TextChannel).bulkDelete(MESSAGES_TO_DELETE, true);
+      await message.channel.send({ content: `Foram limpas ${MESSAGES_TO_DELETE} mensagens do canal`});
+
+    } catch (error) {
+      console.error(error);
+      message.reply({ content: `${error}` });
+
+    }
+  }
+};
---
+++ src/commands/staff/say.ts
@@ -0,0 +1,33 @@
+import { Client, Message, TextChannel } from "discord.js";
+
+module.exports = {
+	name: 'say',
+	description: 'Broadcast a message to specified channel [STAFF]',
+	category: 'staff',
+	execute: async (client:Client, message:Message, args:Array<string>) => {
+    try {
+      const userPerms = message.member!.guild.me?.permissions.toArray();
+      const hasAdminRole = userPerms?.some((role) => (role == "ADMINISTRATOR"));
+      const CHANNEL_ID = args[0];
+      const MESSAGE_TO_SEND = args.slice(1).join(' ');
+  
+      const channel = client.channels.cache.find((channel) => channel.id === CHANNEL_ID);
+  
+      if (!channel) message.reply('ERRO: Canal não encontrado!');
+  
+      if (!hasAdminRole) {
+        message.reply('ERRO: Não Autorizado!!!');
+        return;
+      }
+  
+      if (channel) {
+        (channel as TextChannel).send(`${MESSAGE_TO_SEND}`);
+        message.reply('Mensagem enviada com Sucesso!');
+        return;
+      }
+
+    } catch (error) {
+      console.error("error");
+    }
+  }
+};
