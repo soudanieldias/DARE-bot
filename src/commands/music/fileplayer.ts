9130d7e158fce1303465cf94d6e817462f3de9db
“soudanieldias” <danieldias.b99@hotmail.com>
2022-07-25 21:47:14 -0300

    feat: add fileplayer command

---
+++ src/commands/music/fileplayer.ts
@@ -0,0 +1,30 @@
+import { Client, Message } from "discord.js";
+import { SoundHandler } from '../../handlers';
+
+module.exports = {
+	name: 'fileplayer',
+	description: 'A command who plays a file in voice channel [Music]',
+	category: 'music',
+	execute: async (_client:Client, message:Message, args:Array<string>) => {
+    try {
+      const voiceChannel = message.member?.voice.channelId;
+      const guildId = message.guildId;
+      const sourceURL = args[0];
+
+      if(!sourceURL) return message.reply('Erro: Forneça um Link para Arquivo MP3!');
+
+      if (!voiceChannel) return message.reply({ content: 'Connect to a voice Channel first.' });
+
+      const connectionParams = {
+        channelId: `${voiceChannel}`,
+        guildId: `${guildId}`,
+        adapterCreator: message.guild?.voiceAdapterCreator,
+      };
+
+      SoundHandler.playSound(sourceURL, connectionParams, false);
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
