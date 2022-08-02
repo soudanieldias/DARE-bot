ac85c5434e882e3a377880655535f1594986e15a
“soudanieldias” <danieldias.b99@hotmail.com>
2022-08-02 17:48:15 -0300

    feat: add DBConnect to events

--- src/events/index.ts
+++ src/events/index.ts
@@ -3,5 +3,13 @@ import SetActivity from './SetActivity';
 import OnReady from './OnReady';
 import OnMessageCreate from './OnMessageCreate';
 import OnInteraction from './OnInteraction';
+import DBConnect from './DBConnect';
 
-export { LoadCommands, SetActivity, OnReady, OnMessageCreate, OnInteraction };
+export {
+  LoadCommands,
+  SetActivity,
+  OnReady,
+  OnMessageCreate,
+  OnInteraction,
+  DBConnect
+};
