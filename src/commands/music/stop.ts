074699e99a28f17f0d7ae0c9fc841603b926b839
“soudanieldias” <danieldias.b99@hotmail.com>
2022-08-13 19:43:44 -0300

    fix: fix stop function & add adapters verification before perform command

--- src/commands/music/stop.ts
+++ src/commands/music/stop.ts
@@ -7,8 +7,12 @@ module.exports = {
   description: 'Para a execução do Player de Música',
   category: 'music',
   execute: async (client:Client, message:Message, _args:Array<string>) => {
+    const voiceChannel = message.member?.voice.channelId;
+
+    if (client.voice.adapters.size < 1) return;
+
     const connectionParams = {
-      channelId: '',
+      channelId: `${voiceChannel}`,
       guildId: message.guildId!,
       adapterCreator: message.guild?.voiceAdapterCreator,
     };
