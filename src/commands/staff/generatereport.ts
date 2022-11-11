3f2b42f362dcd1595bf59c044b5db0d15b91eb1f
Daniel Dias <danieldias.b99@hotmail.com>
2022-11-10 23:30:32 -0300

    feat: implements new generatereport command

--- src/commands/staff/generatereport.ts
+++ src/commands/staff/generatereport.ts
@@ -1,42 +1,51 @@
-import { Client, Message, PermissionFlagsBits } from "discord.js";
+import { Client, Interaction, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
 import fs from 'fs/promises';
 
 module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('generatereport')
+    .setDescription('Generate a complete data report of players & Boin into Discord Server [STAFF]'),
   name: 'generatereport',
   description: 'Generate a complete Data report of players & Bots in server [STAFF]',
   category: 'staff',
-  execute: async (client:Client, message:Message) => {
-    const hasAdminRole = message.member!.permissions.has([PermissionFlagsBits.Administrator]);
+  execute: async (client:Client, interaction:Interaction) => {
+    const hasAdminRole = interaction.memberPermissions?.has([PermissionFlagsBits.Administrator]);
 
-    if(!hasAdminRole) return message.reply('Erro: Não Autorizado!');
+    if (interaction.isRepliable())
+    {
+      if(!hasAdminRole || !interaction.guildId) {
+        return interaction.reply('Erro: Não Autorizado!');
+      }
 
-    const guild = client.guilds.resolve(message.guildId!);
-    const guildMembers = await guild!.members.fetch();
-    const [ users, bots ] = [
-      guildMembers.filter((member) => !member.user.bot),
-      guildMembers.filter((member) => member.user.bot),
-    ];
+      const guild = client.guilds.resolve(interaction.guildId!);
+      const guildMembers = await guild!.members.fetch();
+      const [ users, bots ] = [
+        guildMembers.filter((member) => !member.user.bot),
+        guildMembers.filter((member) => member.user.bot),
+      ];
 
-    const timeStamp = new Date().getTime();
-    
-    const botMessage = await message.channel.send({content: 'Iniciando a criação do Relatório de Membros do servidor'});
+      const timeStamp = new Date().getTime();
 
-    // Cria Diretório
-    await fs.mkdir(`./src/reports/${timeStamp}`);
+      const botinteraction = await interaction.reply({ content: 'Iniciando a criação do Relatório de Membros do servidor' });
 
-    // Cria LOG de USUÁRIOS
-    await fs.writeFile(`./src/reports/${timeStamp}/users.txt`, `REPORT DE USUÁRIOS: ${timeStamp}\n`);  
-    users.forEach(async (user) => {
-      await fs.appendFile(`./src/reports/${timeStamp}/users.txt`, `${JSON.stringify(user)}\n`);
-    });
+      // Cria Diretório
+      await fs.mkdir(`./src/reports/${timeStamp}`);
 
-    // Cria LOG de BOTS
-    await fs.writeFile(`./src/reports/${timeStamp}/bots.txt`, `REPORT DE BOTS: ${timeStamp}\n`);
-    bots.forEach(async (bot) => {
-      await fs.appendFile(`./src/reports/${timeStamp}/bots.txt`, `${JSON.stringify(bot)}\n`);
-    })
-
-    await botMessage.edit({ content: `Gerando Relatório ${timeStamp}` });
-    botMessage.edit({ content: `Tudo ok! ID do Relatório: ${timeStamp}`});
+      // Cria LOG de USUÁRIOS
+      await fs.writeFile(`./src/reports/${timeStamp}/users.txt`, `REPORT DE USUÁRIOS: ${timeStamp}\n`);
+      users.forEach(async (user) => {
+        await fs.appendFile(`./src/reports/${timeStamp}/users.txt`, `${JSON.stringify(user)}\n`);
+      });
+  
+      // Cria LOG de BOTS
+      await fs.writeFile(`./src/reports/${timeStamp}/bots.txt`, `REPORT DE BOTS: ${timeStamp}\n`);
+      bots.forEach(async (bot) => {
+        await fs.appendFile(`./src/reports/${timeStamp}/bots.txt`, `${JSON.stringify(bot)}\n`);
+      })
+  
+      await interaction.editReply({ content: `Gerando Relatório ${timeStamp}` });
+  
+      return interaction.editReply({ content: `Tudo ok! ID do Relatório: ${timeStamp}`});
+    }
   }
 };
