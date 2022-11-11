2116571a86671278ed524cb031034781dfbe5983
Daniel Dias <danieldias.b99@hotmail.com>
2022-11-11 00:08:19 -0300

    feat: implements new soundpad command

--- src/commands/music/soundpad.ts
+++ src/commands/music/soundpad.ts
@@ -1,57 +1,59 @@
-import { ActionRowBuilder, ButtonBuilder } from '@discordjs/builders';
-import { Client, Message, TextChannel} from 'discord.js';
+import { ActionRowBuilder, ButtonBuilder, SlashCommandBuilder } from '@discordjs/builders';
+import { ButtonInteraction, Client, CommandInteraction, Interaction, MessageInteraction, TextChannel } from 'discord.js';
 import { SoundHandler } from '../../handlers';
 import { sliceArray, generateButtonsData } from '../../helpers';
 
 module.exports = {
-  name: 'soundpad',
-  description: 'Retorna uma lista de Botões de áudio para usar no canal de voz',
-  category: 'Music',
-  execute: async (client:Client, message:Message, args:Array<string>) => {
+  data: new SlashCommandBuilder()
+    .setName('soundpad')
+    .setDescription('Retorna uma lista de botões de áudio para usar no canal de Voz específico'),
+  category: 'music',
+  execute: async (client:Client, interaction:ButtonInteraction) => {
     try {
-      const voiceChannel = message.member?.voice.channelId;
-      const messageChannel = client.channels.cache.get(message.channelId);
-      const guildId = message.guildId;
-      const adapterCreator = message.guild?.voiceAdapterCreator;
-
-      // Generates an Array of ButtonBuilder instances
-      const fileObjects = await generateButtonsData();
-
-      if (!voiceChannel) {
-        message.channel
-          .send(`<@${message.member?.id}> Você precisa estar em um canal de voz para executar o Soundpad`);
-        return;
-      }
-
-      if (!args[0] && voiceChannel) {
-        // Slices buttons array in sub-arrays of max of 5 elements
-        const slicedResult:Array<Array<ButtonBuilder>> = await sliceArray(fileObjects, 5);
-
-        // Set the array who stores all instances of ActionRowBuilder with 5 buttons each
-        const allRows:Array<ActionRowBuilder<any>> = [];
-
-        // Iteract over all sub-array in slicedResult, and push each ActionRowBuilder instance
-        // with 5 buttons into allRows array
-        slicedResult.forEach((subArray:Array<ButtonBuilder>) => {
-          const actionRowBuilder = new ActionRowBuilder()
-            .addComponents(subArray);
-          allRows.push(actionRowBuilder);
-        })
-
-        return allRows.forEach((rowData, index) => (
-          (messageChannel as TextChannel)
-            .send({ content: `Lista de Áudios: ${index + 1}`, components: [rowData]})
-        ));
-      }
-
-      if (voiceChannel) {
+      if (interaction.isRepliable()) {
+        const guild = client.guilds.cache.get(interaction.guildId!);
+        const member = guild?.members.cache.get(interaction.member!.user.id);
+        const messageChannel = interaction.channel;
+        const voiceChannel = member?.voice.channel?.id;
+        const padName = interaction.customId;
+        const adapterCreator = interaction.guild?.voiceAdapterCreator;
+        
+        if(!voiceChannel) {
+          return interaction.reply(`<@${interaction.member?.user.id}> Você precisa estar em um canal de voz para executar o Soundpad`);
+        }
+        
         const connectionParams = {
           channelId: `${voiceChannel}`,
-          guildId: guildId!,
+          guildId: interaction.guildId!,
           adapterCreator: adapterCreator!,
         };
 
-        SoundHandler.playSound(`./src/audios/${args[0]}.mp3`, connectionParams, false);
+        if (voiceChannel && !padName) {
+          // Generates an Array of ButtonBuilder instances
+          const fileObjects = await generateButtonsData();
+          // Logica para interaction.isButton
+          // Lógica para interaction.isChatInputCommand
+          // Slices buttons array in sub-arrays of max of 5 elements
+          const slicedResult:Array<Array<ButtonBuilder>> = await sliceArray(fileObjects, 5);
+
+          // Set the array who stores all instances of ActionRowBuilder with 5 buttons each
+          const allRows:Array<ActionRowBuilder<any>> = [];
+
+          // Iteract over all sub-array in slicedResult, and push each ActionRowBuilder instance
+          // with 5 buttons into allRows array
+          slicedResult.forEach((subArray:Array<ButtonBuilder>) => {
+            const actionRowBuilder = new ActionRowBuilder()
+              .addComponents(subArray);
+            allRows.push(actionRowBuilder);
+          });
+
+          return allRows.forEach((rowData, index) => (
+            (messageChannel as TextChannel)
+              .send({ content: `Lista de Áudios: ${index + 1}`, components: [rowData]})
+          ));
+        }
+
+        return SoundHandler.playSound(`./src/audios/${padName}.mp3`, connectionParams, false);
       }
     } catch (error) {
       console.error(`[Erro]: ${error}`);
