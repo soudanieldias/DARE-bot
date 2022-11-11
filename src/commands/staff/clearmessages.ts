45a0f690b306b791d075cf32bd446c7a60de35ce
Daniel Dias <danieldias.b99@hotmail.com>
2022-11-10 23:02:30 -0300

    feat: implements new clearmessages command

--- src/commands/staff/clearmessages.ts
+++ src/commands/staff/clearmessages.ts
@@ -1,27 +1,31 @@
-import { Client, Message, PermissionFlagsBits, TextChannel } from "discord.js";
+import { Client, CommandInteraction, PermissionFlagsBits, SlashCommandBuilder, TextChannel } from "discord.js";
 
 module.exports = {
-	name: 'clearmessages',
-	description: 'Clear a \'n\' quantity of messages in specified channel [STAFF]',
-	category: 'staff',
-	execute: async (_client:Client, message:Message, args:Array<string>) => {
+  data: new SlashCommandBuilder()
+    .setName('clearmessages')
+    .setDescription('Remove um número \'N\' de mensagens no canal atual. [STAFF]')
+    .addIntegerOption(quantity => (
+      quantity.setName("quantity")
+      .setDescription("Digite a quantidade de mensagens a serem deletadas (Um número entre 1-100).")
+      .setMinValue(1)
+      .setMaxValue(100)
+      .setRequired(true)
+    )),
+  category: 'staff',
+	async execute (_client:Client, interaction:CommandInteraction) {
     try {
-      const hasAdminRole = message.member!.permissions.has([PermissionFlagsBits.Administrator]);
-      const MESSAGES_TO_DELETE = Number(args[0]);
+      if (interaction.isRepliable()) {
+        const hasAdminRole = interaction.memberPermissions?.has([PermissionFlagsBits.Administrator]);
+        const MESSAGES_TO_DELETE = Number(interaction.options.get("quantity")!.value);
 
-      if (!hasAdminRole) return message.reply('ERRO: Não Autorizado!!!');
+        if (!hasAdminRole) return interaction.reply('ERRO: Não Autorizado!!!');
 
-      if(!MESSAGES_TO_DELETE || MESSAGES_TO_DELETE > 100 || MESSAGES_TO_DELETE < 1 ) {
-        return message.reply({ content: 'Quantidade de mensagens à apagar Inválida!' });
+        await (interaction.channel as TextChannel).bulkDelete(MESSAGES_TO_DELETE, true);
+
+        return interaction.reply({ content: `Foram limpas ${MESSAGES_TO_DELETE} mensagens do canal`});
       }
-
-      await (message.channel as TextChannel).bulkDelete(MESSAGES_TO_DELETE, true);
-      await message.channel.send({ content: `Foram limpas ${MESSAGES_TO_DELETE} mensagens do canal`});
-
     } catch (error) {
-      console.error(error);
-      message.reply({ content: `${error}` });
-
+      console.error('[ClearMessages] Error: ', error);
     }
   }
 };
